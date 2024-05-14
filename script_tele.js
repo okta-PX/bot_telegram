b131a1d1c3305a352653f4348befb6521e432166
okta-PX <122891366+okta-PX@users.noreply.github.com>
2024-05-14 16:23:18 +0700

    Update script_tele.js
    UBAH SCRIPT "BARANG"  MENJADI "ITEM"

--- script_tele.js
+++ script_tele.js
@@ -1,48 +1,76 @@
-var token = "";
-var SheetID = "";
+var token = "5367291558:AAHCFsZ-lgsiidvuZ1LuT2THjqjmo7GYJi4";
+var SheetID = "1bhBA3S79R5jlMuSCRz5rsPW86SGQGzCejXj4i7fyDOY";
 
 function doPost(e) {
   var stringJson = e.postData.getDataAsString();
   var updates = JSON.parse(stringJson);
  
-    if(updates.message.text){
-      sendText(updates.message.chat.id,CariBarangDariIDSheet(updates.message.text)); 
-    }
+  if(updates.message.text){
+    sendText(updates.message.chat.id, findItemByID(updates.message.text)); 
+  }
 }
 
-function AmbilSheet1(){
+function getSheet1(){
   var rangeName = 'Sheet1!A2:C';
   var rows = Sheets.Spreadsheets.Values.get(SheetID, rangeName).values;
   return rows;
 }
 
-function AmbilSheet2(){
+function getSheet2(){
   var rangeName = 'Sheet2!A2:C';
   var rows = Sheets.Spreadsheets.Values.get(SheetID, rangeName).values;
   return rows;
 }
 
-function CariBarangDariIDSheet(IDbarang){
-  var dataBarang = AmbilSheet1(); 
-  for (var row = 0; row < dataBarang.length; row++) {
-    if(dataBarang[row][0]==IDbarang){ 
-      return "" + dataBarang[row][2] + "\n" +
-             "" + dataBarang[row][1] ;
-    }
-  }
-
-  var dataBarang = AmbilSheet2(); 
-  for (var row = 0; row < dataBarang.length; row++) {
-    if(dataBarang[row][0]==IDbarang){ 
-      return "" + dataBarang[row][2] + "\n" +
-             "" + dataBarang[row][1] ;
-    }
-  }
-  return "ID tidak Terdaftar, ketik /Site Untuk Menampilkan data 😉"
+function getSheet3(){
+  var rangeName = 'Sheet3!A2:C';
+  var rows = Sheets.Spreadsheets.Values.get(SheetID, rangeName).values;
+  return rows;
 }
 
-function sendText(chatid,text,replymarkup){
-var data = {
+function getSheet4(){
+  var rangeName = 'Sheet4!A2:C';
+  var rows = Sheets.Spreadsheets.Values.get(SheetID, rangeName).values;
+  return rows;
+}
+
+function findItemByID(itemID){
+  var data = getSheet1(); 
+  for (var row = 0; row < data.length; row++) {
+    if(data[row][0] == itemID){ 
+      return "" + data[row][2] + "\n" +
+             "" + data[row][1];
+    }
+  }
+
+  data = getSheet2(); 
+  for (var row = 0; row < data.length; row++) {
+    if(data[row][0] == itemID){ 
+      return "" + data[row][2] + "\n" +
+             "" + data[row][1];
+    }
+  }
+
+  data = getSheet3(); 
+  for (var row = 0; row < data.length; row++) {
+    if(data[row][0] == itemID){ 
+      return "" + data[row][2] + "\n" +
+             "" + data[row][1];
+    }
+  }
+
+  data = getSheet4(); 
+  for (var row = 0; row < data.length; row++) {
+    if(data[row][0] == itemID){ 
+      return "" + data[row][2] + "\n" +
+             "" + data[row][1];
+    }
+  }
+  return "ID tidak Terdaftar. \nKetik /site untuk menampilkan data 😉\nBisa juga lihat tutor di /tutor";
+}
+
+function sendText(chatid, text, replymarkup){
+  var data = {
     method: "post",
     payload: {
       method: "sendMessage",
@@ -54,10 +82,3 @@ var data = {
   };
   UrlFetchApp.fetch('https://api.telegram.org/bot' + token + '/', data);
 }
-
-
-
-
-
-
-
